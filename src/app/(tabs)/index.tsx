7e15c1a2029e6dd643bd78398c54bf7dbd587e2d
sethumadhavan kochukrishnan <sethumadh@gmail.com>
2024-05-28 19:22:38 +1000

    expo router

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,10 +1,17 @@
-import { StyleSheet, View } from "react-native"
+import { FlatList, View } from "react-native"
 import ProductListItem from "@/src/components/ProductListItem"
+import products from "@/assets/data/products"
 
 export default function MenuScreen() {
   return (
-    <View>
-      <ProductListItem />
-    </View>
+    // <View style={{ flex: 1 }}>
+    <FlatList
+      numColumns={2}
+      contentContainerStyle={{ gap: 10 }}
+      columnWrapperStyle={{ gap: 10, padding: 10 }}
+      data={products}
+      renderItem={({ item }) => <ProductListItem product={item} />}
+    />
+    // </View>
   )
 }
--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,18 +1,24 @@
-import products from "@/assets/data/products"
-import { StyleSheet, View, Text, Image } from "react-native"
-import Colors from "../constants/Colors"
+import { StyleSheet, View, Text, Image, Pressable } from "react-native"
+import Colors from "@/src/constants/Colors"
+import { Product } from "../types"
+import { Link } from "expo-router"
 
-function ProductListItem() {
+type ProductListItemProps = {
+  product: Product
+}
+
+function ProductListItem({ product }: ProductListItemProps) {
   return (
-    <>
-      {products.map((product) => (
-        <View  style={styles.container} key={product.id}>
-          <Image style={styles.image} source={{ uri: product.image }} />
-          <Text style={styles.title}>{product.name}</Text>
-          <Text style={styles.price}>{product.price}</Text>
-        </View>
-      ))}
-    </>
+    <Link href={`${product.id}`} asChild>
+      <Pressable style={styles.container} key={product.id}>
+        <Image
+          style={styles.image}
+          source={{ uri: product.image && product.image }}
+        />
+        <Text style={styles.title}>{product.name}</Text>
+        <Text style={styles.price}>{product.price}</Text>
+      </Pressable>
+    </Link>
   )
 }
 
@@ -20,6 +26,7 @@ export default ProductListItem
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    maxWidth: "50%",
     backgroundColor: "white",
     padding: 10,
     borderRadius: 20,
@@ -36,5 +43,6 @@ const styles = StyleSheet.create({
   image: {
     width: "100%",
     aspectRatio: 1,
+    resizeMode: "contain",
   },
 })
